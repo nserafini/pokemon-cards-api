a129b72023fb1037eae853b688be601b317fe6ba
Nicolas Serafini <nicolas.serafini@tunubi.com>
2021-08-10 20:19:30 -0300

    feat: add config & db init

---
+++ api/config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config(object):
+
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASS = os.environ.get("DB_PASS")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_NAME = os.environ.get("DB_NAME")
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASS}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "It works!"
+from api.db import db
+from api.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
+
+    db.init_app(app)
+
+    @app.route('/')
+    def index():
+        return "It works!"
+
+    return app
 
 if __name__ == '__main__':
-    app.run()
+    app = create_app()
+    app.run(host="0.0.0.0", debug=True)
