16f7e9f26c07dc29d849c6344f39f2e9108bdc22
Nicolas Serafini <nicolas.serafini@tunubi.com>
2021-08-13 02:04:59 -0300

    feat: add test client with api key

--- api/utils/testing.py
+++ api/utils/testing.py
@@ -1,17 +1,31 @@
 import unittest
 
 from app import create_app
+from flask import testing
+from werkzeug.datastructures import Headers
 
 from api.db import db
 
 db.session.session_factory.configure(expire_on_commit=False)
 
 
+class TestAuthenticatedClient(testing.FlaskClient):
+    def open(self, *args, **kwargs):
+        api_key_headers = Headers({
+            'Api-Key': 'secret'
+        })
+        headers = kwargs.pop('headers', Headers())
+        headers.extend(api_key_headers)
+        kwargs['headers'] = headers
+        return super().open(*args, **kwargs)
+        
+
 class BaseTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.app = create_app()
+        cls.app.test_client_class = TestAuthenticatedClient
         cls.test_client = cls.app.test_client()
         ctx = cls.app.app_context()
         ctx.push()
